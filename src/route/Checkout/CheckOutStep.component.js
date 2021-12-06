0e9f62e87dcf5c7f9440f377a6ca09bee3914812
Melhem El Dirani <melhemdirani@Melhems-MacBook-Air.local>
2021-12-06 18:42:32 +0200

    first commit

---
+++ src/route/Checkout/CheckOutStep.component.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+class CheckOutStep extends Component {
+    splitName(text) {
+        
+        text.split("_")[0]
+        let b = text.split("_")[0]
+        let c = b.toLowerCase()
+        let d = c.charAt(0).toUpperCase() + c.slice(1);
+
+        return d;
+    }
+    indexOf(input) {
+        return this.props.steps.indexOf(input);
+    }
+    render() {
+       
+        const { steps, currentStep } = this.props
+        return (
+            <div className="step">
+                {steps.map((step, index) =>
+                   
+                    <>
+                        <div 
+                            className={
+                                index <= this.indexOf(currentStep) ? 
+                                "line" 
+                                : index == 2 
+                                ? "displayNone" 
+                                : "gray_line"
+                            }
+                        />
+                        <div className={index == 2 ? "displayNone" :"item"}>
+                            <p className={index <= this.indexOf(currentStep) ? "index"  :"gray_index"}> 
+                                { index < this.indexOf(currentStep) ? "✓"  : index == 2 ? "" : index + 1} 
+                            </p>
+                            <p 
+                                className={
+                                    index > this.indexOf(currentStep) 
+                                    ? "billing dark_gray" 
+                                    : "billing"
+                                }
+                            >{this.splitName(step)}</p>
+                        </div>
+                        <div 
+                            className={
+                                index < this.indexOf(currentStep) ||  this.indexOf(currentStep) === steps.length - 2 
+                                ? "line" 
+                                : index == 2 
+                                ? "displayNone"
+                                : "gray_line"
+                            }
+                        />
+                    </>
+                    
+                )}
+            </div>
+        )
+    }
+}
+
+export default CheckOutStep
---
+++ src/route/Checkout/Checkout.component.js
@@ -0,0 +1,44 @@
+import { Checkout as SourceCheckout } from 'SourceRoute/Checkout/Checkout.component'
+import ContentWrapper from 'Component/ContentWrapper';
+import './Checkout.extention.style';
+import CheckoutStep from './CheckOutStep.component';
+
+
+class Checkout extends SourceCheckout {
+  
+    renderCheckoutSteps()  {
+        const currentStep = this.props.checkoutStep
+        let steps = Object.keys(this.stepMap)
+        return (
+            <CheckoutStep currentStep={currentStep} steps={steps}/>
+
+        )
+
+    }
+    render(){
+        return(
+           <main block="Checkout">
+                { this.renderCheckoutSteps() }
+                <ContentWrapper
+                  wrapperMix={ { block: 'Checkout', elem: 'Wrapper' } }
+                  label={ __('Checkout page') }
+                >
+                    { this.renderSummary(true) }
+                    <div block="Checkout" elem="Step">
+                        { this.renderTitle() }
+                        { this.renderGuestForm() }
+                        { this.renderStep() }
+                        { this.renderLoader() }
+                    </div>
+                    <div>
+                        { this.renderSummary() }
+                        { this.renderPromo() }
+                        { this.renderCoupon() }
+                    </div>
+                </ContentWrapper>
+            </main>
+        )
+    }
+}
+
+export default Checkout;
